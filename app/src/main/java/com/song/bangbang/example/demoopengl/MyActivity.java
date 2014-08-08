0892a66a84393c0beb61742f7ded840141c8b107
guest <guest@tudou.com>
2014-08-08 17:10:00 +0800

    add smooth triangle.

--- app/src/main/java/com/song/bangbang/example/demoopengl/MyActivity.java
+++ app/src/main/java/com/song/bangbang/example/demoopengl/MyActivity.java
@@ -53,10 +53,12 @@ public class MyActivity extends Activity {
 
     public static class MyRender implements GLSurfaceView.Renderer {
 
-        private final Triangle mTriangle;
+        private Triangle mTriangle;
 
         public MyRender() {
+
             mTriangle = new Triangle();
+            mTriangle = new SmoothTriangle(false);
         }
 
         @Override
@@ -70,7 +72,7 @@ public class MyActivity extends Activity {
             gl.glViewport(0, 0, width, height);
 
             // make adjustments for screen ratio
-            // XXX bysong
+            // XXX bysong why we need this ???
             float ratio = (float) width / height;
             gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
             gl.glLoadIdentity();                        // reset the matrix to its default state
@@ -85,7 +87,7 @@ public class MyActivity extends Activity {
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadIdentity();   // reset the matrix to its default state
 
-            // XXX bysong
+            // XXX bysong why we need this ???
             // When using GL_MODELVIEW, you must set the view point
             GLU.gluLookAt(gl, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
@@ -97,14 +99,14 @@ public class MyActivity extends Activity {
         // number of coordinates per vertex in this array
         static final int BYTE_PER_FLOAT = 4;
         static final int COORDS_PER_VERTEX = 3;
-        static float TRIANGLE_COORDINATES[] = {
+        static final float TRIANGLE_COORDINATES[] = {
                 // in counterclockwise order:
                 0.0f,  0.622008459f, 0.0f,// top
                 -0.5f, -0.311004243f, 0.0f,// bottom left
                 0.5f, -0.311004243f, 0.0f // bottom right
         };
 
-        static float COLOR[] = { 0.63671875f, 0.76953125f, 0.22265625f, 0.0f };
+        static final float COLOR[] = { 0.63671875f, 0.76953125f, 0.22265625f, 0.0f };
 
         private FloatBuffer mVertexBuffer;
 
@@ -117,14 +119,68 @@ public class MyActivity extends Activity {
         }
 
         public void draw(GL10 gl) {
+            onPreDraw(gl);
+
+            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
+
+            onPostDraw(gl);
+        }
+
+        protected void onPreDraw(GL10 gl) {
+
             gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 
             gl.glColor4f(COLOR[0], COLOR[1], COLOR[2], COLOR[3]);
             gl.glVertexPointer(COORDS_PER_VERTEX, GL10.GL_FLOAT, 0, mVertexBuffer);
+        }
 
-            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
+        protected void onPostDraw(GL10 gl) {
 
             gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        }
+    }
+
+    public static class SmoothTriangle extends Triangle {
+        static final int COLOR_PER_VERTEX = 4;
+        static final float SMOOTH_COLOR[] = {
+                0.f, 0.f, 1.f, 0.0f,
+                0.f, 1.f, 0.f, 0.0f,
+                1.f, 0.f, 0.f, 0.0f};
+        private final  boolean mSmoooth;
+
+        private FloatBuffer mColorBuffer;
+
+        public SmoothTriangle(boolean smooth){
+            super();
+
+            mSmoooth = smooth;
+
+            ByteBuffer vbb = ByteBuffer.allocateDirect(SMOOTH_COLOR.length * COLOR_PER_VERTEX * BYTE_PER_FLOAT);
+            vbb.order(ByteOrder.nativeOrder());
+            mColorBuffer = vbb.asFloatBuffer();
+            mColorBuffer.put(SMOOTH_COLOR);
+            mColorBuffer.position(0);
+        }
+
+        @Override
+        protected void onPreDraw(GL10 gl) {
+            super.onPreDraw(gl);
+
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+            int shadeMode = GL10.GL_FLAT;
+            if (mSmoooth) {
+                shadeMode = GL10.GL_SMOOTH;
+            }
+            gl.glShadeModel(shadeMode);
+            gl.glColorPointer(COLOR_PER_VERTEX, GL10.GL_FLOAT, 0, mColorBuffer);
+        }
+
+        @Override
+        protected void onPostDraw(GL10 gl) {
+            super.onPostDraw(gl);
+
+
+            gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
         }
     }
 
